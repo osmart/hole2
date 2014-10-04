32ba14ff78e6ce8d3ecd6551dda8592a4463c055
Oliver Smart <osmart@smartsci.uk>
2014-10-04 18:38:50 +0100

    hard code compilier to gfortran. Add banner restriction to not-for-profit.

--- src/makver.f
+++ src/makver.f
@@ -8,8 +8,7 @@ C * must be attached to all copies or extracts of the software.      *
 C *                                                                  *
 C * (c) 1996 Oliver Smart & Birkbeck College, All rights reserved    *
 C * (c) 1997 Oliver Smart                    *
-C *                                                                  *
-C ********************************************************************
+C * (c) 2014 SmartSci Limited                *
       program makver
       implicit none
 C little program to run which writes s/r vertim giving the time of link of
@@ -28,8 +27,12 @@ C Adapted 21/11/97 to give who linked it as well
      &'      IMPLICIT NONE',
      &'C s/r which gives the date of linking written by program makver',
      &'      INTEGER		RESOUT',
-     &'      WRITE(RESOUT, ''(A/ A,A/ A,A)'') ',
-     &'     &'' HOLE release 2.2.002 (12 Jan 2005)'',', 
+     &'      WRITE(RESOUT, ''(A)'') ',
+     &'     &'' '',', 
+     &'     &'' For help on HOLE suite see''//',
+     &'     &''  http://www.smartsci.uk/hole/'',',
+     &'     &'' '',', 
+     &'     &'' HOLE release 2.2.003 (04 Oct 2014)'',', 
      &'     &'' Program linked at '//line(1:index(line,'   ')-1)//''',' 
 
 C find out who linked it
@@ -40,46 +43,24 @@ C find out who linked it
 !      call system('rm makver.temp_file')
 
 ! nov 2004 take out last modified (no source dist)
-      if (index(line,'osmart').ne.0) line = 'HOLE_DIST'
-      write( 1, '(a)')
+      if (index(line,'osmart').ne.0) then
+         write( 1, '(a)')
+     &'     &'' built for hole web site binary download '',',
+     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
+     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
+     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
+     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
+     &'     &'' ''' 
+      else
+         write( 1, '(a)')
      &'     &'' by user '//line(1:index(line,'   ')-1)//''','
-      write( 1, '(a)')
-     &'     &'' For help on hole suite see'',',
-     &'     &''  http://hole.biop.ox.ac.uk/hole/help'''
-!      WRITE( NOUT,  '(A)')
-!     &' For help on hole suite see ',
-!     &' http://hole.biop.ox.ac.uk/hole/help'
-
-!      write( 1, '(a)')
-!     &'     &'' By user '//line(1:20)//''',',
-!     &'     &'' Last modified .f files: '''
-
-!C now add bit which gives the last modified fortran file
-!      call system('ls -lt *.f > makver.temp_file')
-!      open( 2, file='makver.temp_file', status='old')
-!C n.b. the last modified fortran files will always be vertim!
-!C so read second line
-!      read( 2, '(a)') line
-!      read( 2, '(a)') line
-!      write( 1, '(a)')
-!     &'      WRITE(RESOUT, ''(A)'') ',
-!     &'     &''     '//line(35:70)//''''
-!C give last 3 modified files
-!      read( 2, '(a)') line
-!      write( 1, '(a)')
-!     &'      WRITE(RESOUT, ''(A)'') ',
-!     &'     &''     '//line(35:70)//''''
-!      read( 2, '(a)') line
-!      write( 1, '(a)')
-!     &'      WRITE(RESOUT, ''(A)'') ',
-!     &'     &''     '//line(35:70)//''''
-
+      endif
       write( 1, '(a)')
      &'      RETURN',
      &'      END'
       close(1)
-C compile this s/r
-      call system('$FC -c -O vertim.f')
+C compile this s/r need to know the compilier for now hardcode
+      call system('gfortran -c -O vertim.f')
 C hole version
       call system('ar rv hole.a vertim.o')
       call system('rm makver.temp_file vertim.o')
