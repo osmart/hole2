bb422f07c951b268bc603c218ef0eb6bbb0501e8
Oliver Smart <osmart@smartsci.uk>
2015-03-24 08:23:58 +0000

    for OSX the directory must be last

--- tarball.sh
+++ tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # command used to packup distribution tar ball
 cd ..
-tar cvzf hole2/hole2.2.x.x.tar.gz hole2 --exclude README.md  --exclude src \
+tar cvzf hole2/hole2.2.x.x.tar.gz --exclude README.md  --exclude src \
    --exclude 'hole*.tar.gz' --exclude index.asciidoc --exclude .ignore \
-   --exclude tarball.sh --exclude .git --exclude .gitignore
+   --exclude tarball.sh --exclude .git --exclude .gitignore hole2
