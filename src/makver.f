c589c76b6e9e829df7b9734d8e5f0cdfbcee7b51
Oliver Smart <osmart@smartsci.uk>
2015-03-24 11:55:35 +0000

    environment variable for the initial splash message. Working version for Not-for-profit, Amgen and possible source distribution.

--- src/makver.f
+++ src/makver.f
@@ -1,80 +1,57 @@
 ! (c) 1996 Oliver Smart & Birkbeck College, All rights reserved
 ! (c) 1997 Oliver Smart 
 ! (c) 2014,2015 SmartSci Limited 
-      program makver
-      implicit none
+      PROGRAM MAKVER
+      IMPLICIT NONE
 ! little program to run which writes s/r vertim giving 
 ! header information for the hole executable and other programs
-      character*80 line
       CHARACTER*300 HoleVersion, HoleDate, HoleBuild, HoleBuildDetail
+      CHARACTER*300 HoleRestrict
 ! build information for proper release from environment variables
       CALL GET_ENVIRONMENT_VARIABLE( "HoleVersion", HoleVersion)
       CALL GET_ENVIRONMENT_VARIABLE( "HoleDate", HoleDate)
       CALL GET_ENVIRONMENT_VARIABLE( "HoleBuild", HoleBuild)
       CALL GET_ENVIRONMENT_VARIABLE( "HoleBuildDetail", HoleBuildDetail)
-      IF (LEN_TRIM(HoleVersion).LE.1) HoleVersion = "?.?.?"
-      IF (LEN_TRIM(HoleDate).LE.1) HoleDate = "date unknown"
+      CALL GET_ENVIRONMENT_VARIABLE( "HoleRestrict", HoleRestrict)
+      IF (LEN_TRIM(HoleVersion).LE.1) HoleVersion = 
+     &"SOURCE DISTRIBUTION"
+      IF (LEN_TRIM(HoleDate).LE.1) HoleDate = "?"
       IF (LEN_TRIM(HoleBuild).LE.1) HoleBuild = "3rd party build"
       IF (LEN_TRIM(HoleBuildDetail).LE.1) HoleBuildDetail = 
-     &"3rd party build, Strictly no distribution to others\n" //
-     &"3rd party build, Strictly no distribution to others\n" //
-     &"3rd party build, Strictly no distribution to others\n" 
+     &"3rd party build, Strictly no distribution to others." 
+      IF (LEN_TRIM(HoleRestrict).LE.1) HoleRestrict = 
+     &"Not to be used without a signed license agreement"
 
-      WRITE(*,*) 'debug HoleVersion= ', HoleVersion
-      WRITE(*,*) 'debug HoleDate= ', HoleDate
-      WRITE(*,*) 'debug HoleBuild= ', HoleBuild
-      WRITE(*,*) 'debug HoleBuildDetail= ', HoleBuildDetail
-
-
-      call system('date> makver.temp_file')
-      call system('rm vertim.f')
-      open( 1, file='vertim.f', status='new')
-      open( 2, file='makver.temp_file', status='old')
-      read( 2, '(a)') line
-      close(2)
-      call system('rm makver.temp_file')
-
-
-      write( 1, '(a)')
+      CALL SYSTEM('rm -f vertim.f')
+      OPEN( 1, FILE='vertim.f', STATUS='NEW')
+      WRITE( 1, '(A)')
      &'      SUBROUTINE VERTIM( RESOUT)',
      &'      IMPLICIT NONE',
      &'C s/r which gives the date of linking written by program makver',
-     &'      INTEGER		RESOUT',
+     &'      INTEGER RESOUT',
      &'      WRITE(RESOUT, ''(A)'') ',
      &'     &'' '',', 
      &'     &'' For help on HOLE suite see''//',
      &'     &''  http://www.smartsci.uk/hole/'',',
+     &'     &'' '',' 
+
+      WRITE( 1, '(A)')
+     &'     &'' HOLE release '//TRIM(HoleVersion)//' ('//
+     &                                  TRIM(HoleDate)//') '', '
+      WRITE( 1, '(A)')
      &'     &'' '',', 
-     &'     &'' HOLE release 2.2.004 (15 Oct 2014)'',', 
-     &'     &'' Program linked at '//line(1:index(line,'   ')-1)//''',' 
+     &'     &'' '//TRIM(HoleBuildDetail)//' '', '
+      WRITE( 1, '(A)')
+     &'     &'' '//TRIM(HoleRestrict)//' '' '
 
-! find out who linked it
-      call system('echo $USER > makver.temp_file')
-      open( 2, file='makver.temp_file', status='old')
-      read( 2, '(a)') line
-      close(2)
-!      call system('rm makver.temp_file')
-
-! nov 2004 take out last modified (no source dist)
-      if (index(line,'osmart').ne.0) then
-         write( 1, '(a)')
-     &'     &'' built for http://www.smartsci.uk/hole/''//',
-     &'     &'' not-for-profit version binary download '',',
-     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
-     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
-     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '',',
-     &'     &'' RESTRICTED TO NOT-FOR-PROFIT USAGE ONLY. '''
-      else
-         write( 1, '(a)')
-     &'     &'' by user '//line(1:index(line,'   ')-1)//''','
-      endif
-      write( 1, '(a)')
+      WRITE( 1, '(A)')
      &'      RETURN',
      &'      END'
-      close(1)
+      CLOSE(1)
+
 ! compile this s/r need to know the compilier for now hardcode
       call system('gfortran -c -O vertim.f')
 ! hole version
       call system('ar rv hole.a vertim.o')
-      call system('rm makver.temp_file vertim.o')
+      call system('rm vertim.o')
       end
