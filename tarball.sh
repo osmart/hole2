1211f85f8921f471632cff8f5b56c5ec36e1a7c6
Oliver Smart <osmart@smartsci.uk>
2015-03-24 08:54:34 +0000

    tarball name for specific builds See https://github.com/osmart/hole2/wiki/201503-Build-v004

--- tarball.sh
+++ tarball.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # command used to packup distribution tar ball
+UNAME=`uname`-`uname -m` # for instance Linux-x86_64
+FNAME=hole2/hole2
+FNAME+=-$HoleBuild # for instance NotForProfit
+FNAME+=-$HoleVersion # for instance 2.004
+FNAME+=-$UNAME  # for instance Linux-x86_64
+FNAME+=.tar.gz 
+echo "tarball name $FNAME"
 cd ..
-tar cvzf hole2/hole2.2.x.x.tar.gz --exclude README.md  --exclude src \
-   --exclude 'hole*.tar.gz' --exclude index.asciidoc --exclude .ignore \
-   --exclude tarball.sh --exclude .git --exclude .gitignore hole2
+tar cvzf $FNAME --exclude README.md  --exclude src \
+   --exclude 'hole2/*.tar.gz' --exclude index.asciidoc --exclude .ignore \
+   --exclude tarball.sh --exclude .git --exclude .gitignore --exclude 'hole2/source*' \
+   hole2
